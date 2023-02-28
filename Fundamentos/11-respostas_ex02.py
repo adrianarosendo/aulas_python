8553d0fe8787797235b52d04ab369496ba8e95e6
adrianarosendo <adriana.maiate@gmail.com>
2023-02-27 21:21:20 -0300

    Aulas Operadores

--- Fundamentos/11-respostas_ex02.py
+++ Fundamentos/11-respostas_ex02.py
@@ -1,11 +1,16 @@
+#Estudos
 estudo_terca = False
 estudo_quinta = False
 
+#Resultados
 nota10 = estudo_quinta and estudo_terca
 nota8 = estudo_terca != estudo_quinta
 passeio = estudo_terca or estudo_quinta
 ficar_em_casa = not passeio
 reprovado = not passeio
 
+#Impressão
 print("Nota 10={} Nota 8={} Passeio={} Ficar em casa={} Reprovado={}"
       .format(nota10, nota8, passeio, ficar_em_casa, reprovado))
+
+#interpolação .format
---
+++ Fundamentos/12-operadores_ternarios.py
@@ -0,0 +1,12 @@
+final_de_semana = False
+
+print('Vou para o colégio = ' + ('sim.', 'não.')[final_de_semana])
+
+
+print('Vou para o colégio = ' + ('não.' if final_de_semana else 'sim.'))
+
+#Mais Operadores
+
+lista = [1,2,3,'Adriana', 'Colégio' ]
+print(2 in lista)
+print('Ana' not in lista)
