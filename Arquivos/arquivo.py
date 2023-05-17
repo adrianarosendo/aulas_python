391bae86e3c13ef7e68a5c850500d3ace4f37bbe
adrianarosendo <adriana.maiate@gmail.com>
2023-05-17 14:41:28 -0300

    inserindo aulas

---
+++ Arquivos/arquivo.py
@@ -0,0 +1,13 @@
+
+#cria um arquivo caso não exista
+#ou substitui o atual
+arquivo = open('arquivo.txt', 'w')
+
+#mostra um erro quando um arquivo não exista
+#modo somente leitura
+arquivo = open('arquivo.txt', 'r')
+
+#mostra um erro quando um arquivo não exista
+#modo somente escrita
+arquivo = open('arquivo.txt', 'x')
+arquivo.close()
