e6c7554924272c1804d218aa3a22713db455d10f
adrianarosendo <adriana.maiate@gmail.com>
2023-05-17 17:26:00 -0300

    incluindo aulas

---
+++ Arquivos/nomes.py
@@ -0,0 +1,36 @@
+#Vamos criar uma variável para ler um arquivo
+arquivo = open('nomes.txt', 'r')
+linhas = arquivo.read()
+arquivo.close()
+#essa variável vai ser do tipo str
+print(linhas)
+
+#novamente lendo o arquivo e usando o readlines
+arquivo = open('nomes.txt', 'r')
+linhas = arquivo.readlines()
+arquivo.close()
+#essa variável vai ser do tipo list
+print(linhas)
+
+#imprimindo em lista
+arquivo = open('nomes.txt', 'r')
+linhas = arquivo.readlines()
+arquivo.close()
+for linha in linhas:
+    print(linha)
+#essa variável vai ser do tipo list
+print(linhas)
+
+#Fazendo calculos com valores do arquivo
+soma = 0
+arquivo = open('nomes.txt', 'r')
+linhas = arquivo.readlines()
+arquivo.close()
+for linha in linhas:
+    pessoas = linha.split(' ')
+    idade = int(pessoas[-1])
+    print(int(idade))
+    soma = soma+idade
+    media = (soma) / len(linhas)
+    
+print(media)
