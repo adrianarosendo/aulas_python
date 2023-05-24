8fe2c776a0ad46a594fb0668490b76d9b923779f
adrianarosendo <adriana.maiate@gmail.com>
2023-05-24 19:34:19 -0300

    inserindo projetos

--- Dinamica/01-jogo_pedra_papel_tesoura.py
+++ Dinamica/01-jogo_pedra_papel_tesoura.py
@@ -1,19 +1,21 @@
 import random
+
+#inserindo as bases 
 print("Let's start a play, rock, paper and scissors!")
-user = input("What's your choise: rock, paper or scissors! ")
+user = input("What's your choise: rock, paper or scissors: ")
 computer = random.choice(["rock", "paper", "scissors"])
 
 #função que chama a partida
-def play(user2, computer2):
-    print("I chose: ", computer2)
-    if user2 == computer2:
+def play(player1, player2):
+    print("I chose: ", player2)
+    if player1 == player2:
         return print("It's a tie!")
-    quem_venceu(user2, computer2)
+    who_win(player1, player2)
 
 #função que define quem venceu
-def quem_venceu(user1, computer1):
-    if (user1 == "rock" and computer1 == "scissors") or (user1 == "scissors" and computer1 == "paper") \
-        or (user1 =="paper" and computer1 == "rock"):
+def who_win(p, c):
+    if (p == "rock" and c == "scissors") or (p == "scissors" and c == "paper") \
+        or (p =="paper" and c == "rock"):
         return print("You won, congratulations!")
     else:
         return print("You lost, try again!")
