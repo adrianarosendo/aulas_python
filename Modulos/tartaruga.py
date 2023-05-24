207cd618731adc17519158287dcd0053683af9dd
adrianarosendo <adriana.maiate@gmail.com>
2023-05-24 19:29:20 -0300

    inserindo mais atividades

---
+++ Modulos/tartaruga.py
@@ -0,0 +1,17 @@
+import turtle
+  
+t = turtle.Turtle()
+ 
+s = int(input("Enter the length of the side of the Squre: "))
+ 
+t.forward(s) 
+t.left(90) 
+ 
+t.forward(s) 
+t.left(90) 
+ 
+t.forward(s) 
+t.left(90) 
+ 
+t.forward(s) 
+t.left(90) 
