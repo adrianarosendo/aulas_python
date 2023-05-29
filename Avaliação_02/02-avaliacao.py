79378e064ec19ed87bcd54b30ce33833cb0c2019
adrianarosendo <adriana.maiate@gmail.com>
2023-05-29 19:21:35 -0300

    inserindo aulas

---
+++ Avaliação_02/02-avaliacao.py
@@ -0,0 +1,15 @@
+
+def calcula_area_circulo(r, pi=3.14):
+    return pi*(r*r)
+
+calcula_area_circ = lambda r, pi = 3.14: pi * (r*r)
+
+print(calcula_area_circulo(8,3))
+
+
+print(calcula_area_circulo(8))
+
+
+print(calcula_area_circ(8,3))
+
+print(calcula_area_circ(8))
