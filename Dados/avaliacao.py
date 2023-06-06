0fcbb064daa6919cf6526196575dabbe1215cdef
adrianarosendo <adriana.maiate@gmail.com>
2023-06-06 16:55:12 -0300

    ajustes nos nomes

--- Dados/avaliacao.py
+++ Dados/avaliacao.py
@@ -1,4 +1,3 @@
-#import numpy as np
 import pandas as pd
 
 df = pd.read_csv("https://pycourse.s3.amazonaws.com/bike-sharing.csv")
@@ -6,35 +5,35 @@ df = pd.read_csv("https://pycourse.s3.amazonaws.com/bike-sharing.csv")
 print("Tamanho do df", df.shape)
 
 mean_df = df['windspeed'].mean()
-print("média da coluna windspeed", mean_df)
+print("Média da coluna windspeed", mean_df)
 
 mean_df = df['temp'].mean()
-print("média da coluna temp", mean_df)
+print("Média da coluna temp", mean_df)
 
-print("registros existem para o ano de 2011 - 0 e 2012 - 1\n", df['year'].value_counts() )
+print("Registros existem para o ano de 2011 - 0 e 2012 - 1\n", df['year'].value_counts() )
 
 count_total = df.groupby('year').sum().filter(["year", "total_count"])
 df['year'] = df.groupby('total_count')['total_count'].transform('count').filter(["year", "total_count"])
-print("totais de alugueis de bicicleta por ano: \n",count_total)
+print("Totais de alugueis de bicicleta por ano: \n",count_total)
 #print(df.head(10))
 #print(df.shape)
 #print(df.describe())
 
 mean_estações = df.groupby('season').mean().filter(["season", "total_count"])
-print("média por estação: \n",mean_estações)
+print("Média por estação: \n",mean_estações)
 
 mean_horario= df.groupby('hour').mean().sort_values(["total_count","hour"],
     ascending=True).filter(["hour", "total_count"])
-print("média de locação por horário:\n ",mean_horario)
+print("Média de locação por horário:\n ",mean_horario)
 
 mean_dia_semana= df.groupby('weekday').mean().sort_values(["total_count","weekday"],
     ascending=True)
-print("média de locação por horário dia semana:\n ",mean_dia_semana)
+print("Média de locação por horário dia semana:\n ",mean_dia_semana)
 
 mean_horario_semana= df.query('weekday==3').groupby('hour').mean().sort_values(["total_count","hour"],
     ascending=True)
-print("média de locação por horário semana quarta:\n ",mean_horario_semana)
+print("Média de locação por horário semana quarta:\n ",mean_horario_semana)
 
 mean_horario_semana= df.query('weekday==6').groupby('hour').mean().sort_values(["total_count","hour"],
     ascending=True)
-print("média de locação por horário semana sabado:\n ",mean_horario_semana)
+print("Média de locação por horário semana sabado:\n ",mean_horario_semana)
