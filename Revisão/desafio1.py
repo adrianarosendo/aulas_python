381e47d283a61716c8d2792eda132b69d3cb85c0
adrianarosendo <adriana.maiate@gmail.com>
2023-07-31 17:30:57 -0300

    Revisão desafios 1 e 2

---
+++ Revisão/desafio1.py
@@ -0,0 +1,7 @@
+#Crie um programa que receba duas variáveis (nome e idade) e 
+#imprima essa informação no console
+
+nome = input("Insira seu nome: ")
+idade = input("Insira sua idade:")
+
+print(f"Seu nome é {nome}, e sua idade é {idade} anos")
---
+++ Revisão/desafio2.py
@@ -0,0 +1,11 @@
+#Crie um programa que calcule dois número enviados pelo
+#usuário e imprima a soma, subtração, divisão e multiplicação
+#deles
+
+num1 = float(input("Digite o primeiro número: "))
+num2 = float(input("Digite o segundo número: "))
+
+print(f"Soma: {num1 + num2}")
+print(f"Subtração: {num1 - num2}")
+print(f"Multiplicação: {num1 * num2}")
+print(f"Divisão: {num1 / num2}")
