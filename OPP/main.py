88bb66fcfea99c8edb07b9be1dbcc6af0f2fd461
adrianarosendo <adriana.maiate@gmail.com>
2023-08-09 16:22:27 -0300

    listando pessoa

--- OPP/main.py
+++ OPP/main.py
@@ -1,5 +1,5 @@
 from pessoa import Pessoa
-
+lista_pessoa = []
 pessoa1 = Pessoa("Luiz", 35)
 pessoa2 = Pessoa("Ana", 42)
 
@@ -17,6 +17,12 @@ pessoa1.parar_de_comer()
 pessoa1.falar("Olá classe!")
 pessoa1.ano_de_nascimento()
 print(pessoa1.gera_id())
+print(pessoa2.gera_id())
+lista_pessoa.append(pessoa1)
+lista_pessoa.append(pessoa2)
+
+for pessoa in lista_pessoa:
+    print(f"{pessoa.nome}, {pessoa.idade}")
 
 #print(pessoa1.nome, p2.nome)
 
